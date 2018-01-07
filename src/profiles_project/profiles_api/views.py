175385007d2a46fc90a94ffbc5111743d5b6ec2e
kiwi7 <davidmurphy77@gmail.com>
2018-01-07 22:39:03 +0100

    lesson 31 put, patch, delete apis

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -37,3 +37,18 @@ class HelloApiView(APIView):
          else:
             return Response(
                 serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+     def put(self, request, pk=None):
+         """Handles updates."""
+
+         return Response({'method': 'put'})
+
+     def patch(self, request, pk=None):
+         """Patch only updates fields provided in the request."""
+
+         return Response({'method': 'patch'})
+
+     def delete(self, request, pk=None):
+         """Deletes the object."""
+
+         return Response({'method': 'delete'})
