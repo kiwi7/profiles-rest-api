7678b5e3e523560fc8414d7b78869e7753bc9664
kiwi7 <davidmurphy77@gmail.com>
2018-01-08 09:47:53 +0100

    =Lesson 62 improved feed item API permissions

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -9,6 +9,7 @@ from rest_framework import filters
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 
 from . import serializers
 from . import models
@@ -139,7 +140,7 @@ class UserProfileFeedViewSet(viewsets.ModelViewSet):
      authentication_classes = (TokenAuthentication,) # tuple immutable
      serializer_class = serializers.ProfileFeedItemSerializer
      queryset = models.ProfileFeedItem.objects.all()
-     permission_classes = (permissions.PostOwnStatus, IsAuthenticatedOrReadOnly)
+     permission_classes = (permissions.PostOwnStatus, IsAuthenticated)
 
      def perform_create(self, serializer):
          """Set the user profile to the logged in user."""
