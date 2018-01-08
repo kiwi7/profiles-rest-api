9030f59604e22ebb639ed2bd9d2b3e7c7836bf3c
kiwi7 <davidmurphy77@gmail.com>
2018-01-08 09:39:02 +0100

    =Lesson 60 feed permissions

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -8,6 +8,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from . import serializers
 from . import models
@@ -138,6 +139,7 @@ class UserProfileFeedViewSet(viewsets.ModelViewSet):
      authentication_classes = (TokenAuthentication,) # tuple immutable
      serializer_class = serializers.ProfileFeedItemSerializer
      queryset = models.ProfileFeedItem.objects.all()
+     permission_classes = (permissions.PostOwnStatus, IsAuthenticatedOrReadOnly)
 
      def perform_create(self, serializer):
          """Set the user profile to the logged in user."""
